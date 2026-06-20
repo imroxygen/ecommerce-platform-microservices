1bb3cfa41964e2283592169fb7a7eff826db0857
myoxygen <mroxygen2000@gmail.com>
2026-06-20 09:23:10 +0530

    docker setup

---
+++ infrastructure/mysql/init.sql
@@ -0,0 +1,5 @@
+-- infrastructure/mysql/init.sql
+CREATE DATABASE IF NOT EXISTS product_db;
+CREATE DATABASE IF NOT EXISTS auth_db;
+
+-- You can also add table creation or initial data inserts here if needed later
